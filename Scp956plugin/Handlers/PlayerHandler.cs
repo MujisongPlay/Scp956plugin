5ff334e301b2cd2f19d347102b89ca8b52385aa9
MujisongPlay <96275409+MujisongPlay@users.noreply.github.com>
2023-01-25 09:20:06 +0900

    Add files via upload

---
+++ Scp956plugin/Handlers/PlayerHandler.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exiled.Events.EventArgs.Player;
+using SCP956Plugin.SCP956;
+
+namespace SCP956Plugin.Handlers
+{
+    public class PlayerHandler
+    {
+        bool IsAllowed(ReferenceHub hub)
+        {
+            if (SCP956Plugin.Instance.Config.LockInventory)
+            {
+                foreach (SCP956AI aI in SchematicHandler.aIs)
+                {
+                    if (aI.Targeted.Contains(hub))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public void ChangeItem(ChangingItemEventArgs ev)
+        {
+            if (!IsAllowed(ev.Player.ReferenceHub))
+            {
+                ev.IsAllowed = false;
+            }
+        }
+
+        public void DropItem(DroppingItemEventArgs ev)
+        {
+            if (!IsAllowed(ev.Player.ReferenceHub))
+            {
+                ev.IsAllowed = false;
+            }
+        }
+
+        public void InteractDoor(InteractingDoorEventArgs ev)
+        {
+            if (!IsAllowed(ev.Player.ReferenceHub))
+            {
+                ev.IsAllowed = false;
+            }
+        }
+
+        public void InteractElevator(InteractingElevatorEventArgs ev)
+        {
+            if (!IsAllowed(ev.Player.ReferenceHub))
+            {
+                ev.IsAllowed = false;
+            }
+        }
+
+        public void InteractLocker(InteractingLockerEventArgs ev)
+        {
+            if (!IsAllowed(ev.Player.ReferenceHub))
+            {
+                ev.IsAllowed = false;
+            }
+        }
+    }
+}
---
+++ Scp956plugin/Handlers/SchematicHandler.cs
@@ -0,0 +1,33 @@
+﻿using MapEditorReborn.Events.EventArgs;
+using SCP956Plugin.SCP956;
+using System;
+using System.Collections.Generic;
+
+namespace SCP956Plugin.Handlers
+{
+    class SchematicHandler
+    {
+        public static List<SCP956AI> aIs = new List<SCP956AI> { };
+
+        public void OnSpawn(SchematicSpawnedEventArgs ev)
+        {
+            if (ev.Name.Equals("scp956", StringComparison.InvariantCultureIgnoreCase))
+            {
+                SCP956AI aI = ev.Schematic.gameObject.AddComponent<SCP956AI>();
+                if (aI.gameObject.transform.position.y == -300f)
+                {
+                    aI.SpecialConstruction();
+                }
+                aIs.Add(aI);
+            }
+        }
+
+        public void OnDestroy(SchematicDestroyedEventArgs ev)
+        {
+            if (ev.Name.Equals("scp956", StringComparison.InvariantCultureIgnoreCase) && ev.Schematic.gameObject.TryGetComponent<SCP956AI>(out SCP956AI aI))
+            {
+                aIs.Remove(aI);
+            }
+        }
+    }
+}
